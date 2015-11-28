4a5b93484e0a3ecd623a665d2fb1d5ecc7668d36
Sebastian Florian <rebardesigndk@gmail.com>
2015-11-28 16:36:57 +0100

    feature ( Main Controller ) Dynamically replace data

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,46 @@
  * Controller of the translateApp
  */
 angular.module('translateApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
+
+  .controller('MainCtrl', ['$scope', '$interval', '$translate', function ($scope, $interval, $translate) {
+    $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+    
+    $scope.angularVersionData = {
+    version: angular.version.full
+    };
+    
+    $scope.countData = {
+      count : 0
+    };
+  
+    
+    $translate('ANGULAR_VERSION', $scope.angularVersionData).then(function (angularVersion) {
+      $scope.angularVersion = angularVersion;
   });
+    
+    var startCounting = function() {
+      
+      var go = $interval(function () {
+        if ($scope.countData.count < 10) {
+          $scope.countData.count = $scope.countData.count + 1; 
+        } else {
+          stopCounting();
+        }
+      }, 1000);
+     
+     var stopCounting = function() {
+      $interval.cancel(go);
+      
+      $scope.count = 0;
+      
+      startCounting();
+     };
+    };
+     
+     
+    startCounting();
+  }]);
